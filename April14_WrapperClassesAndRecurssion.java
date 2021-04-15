4c7aae9287759ac989c5889a12dbc36a62710fb4
TutorPatil <30532209+TutorPatil@users.noreply.github.com>
2021-04-15 20:18:39 +0530

    April-15-JavaCode
    April-15-JavaCode

--- April14_WrapperClassesAndRecurssion.java
+++ April14_WrapperClassesAndRecurssion.java
@@ -2,8 +2,37 @@ package com.corejava;
 
 public class April14_WrapperClassesAndRecurssion {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
+		System.out.println(findFactorial(5));
+	}	
 	
+	public static void printNumbersDesc(int num)
+	{
+		System.out.print(num+" ");
+		if(num >0)   // base case
+			printNumbersDesc(num-1);
+	}
+	
+	public static void printNumbersAsc(int num)
+	{
+		if(num >0)   // base case
+			printNumbersAsc(num-1);
+		
+		System.out.print(num+" ");
+		
+	}
+	
+	public static int findFactorial(int num)
+	{
+		if ( num == 1)  // base case
+			return 1;
+		
+		return (num * findFactorial(num-1));
+		
+		
+	}
+	
+	/*
 		int x = 20;
 		
 		Integer xObj = new Integer(x);  // Boxing or wrapping
@@ -22,11 +51,42 @@ public class April14_WrapperClassesAndRecurssion {
 		
 		String s = "1234";
 		
-		int x1 = 1234;
+		int y1 = Integer.parseInt(s);
 		
-		System.out.println(String.valueOf(x1).length());
+		System.out.println(y1+100);
+		
+		System.out.println("==================");
+		
+		char c = 'A';
+		
+		Character c1 = new Character(c); // Boxing
+		
+		c1.charValue(); // Unboxing
+		
+		System.out.println(c1.toString().length());
+		
+		System.out.println(Character.getNumericValue(c));
+		
+		
+		double d = 100.25;
+		Double d1 = new Double(d);
+		
+		System.out.println(d1.intValue());
+		
+		
+		Integer m =  25; // Auto Boxing..	
+		int n = m;  // Unboxing...
+		
+	*/	
+		
+		
+		
+		
+		
+		
+		
 		
 
-	}
+	
 
 }
